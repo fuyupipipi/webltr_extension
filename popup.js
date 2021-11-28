0415e7e1bf81ad19af1b47bc9d004101c56d6a7b
fpip <s1811494@klis.tsukuba.ac.jp>
2021-11-28 22:44:46 +0900

    open eval with new window

--- popup.js
+++ popup.js
@@ -6,3 +6,17 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tab_data) => {
     document.getElementById("title-field").value = title;
     document.getElementById("url-field").value = url;
   });
+  function webappLink(url) {
+    var title = document.getElementById('title-field').value,
+    url = document.getElementById('url-field').value,
+    webapp_url = "https://webltr-webapp.herokuapp.com/webeval/post",
+    target_url = webapp_url + "?url=" + url + "&title=" + title,
+    width = screen.width / 2,
+    height = screen.height,
+    option = "width=" + width + ", height=" + height;
+    window.open(target_url, null, option)
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('.btn').addEventListener('click', webappLink);
+});
